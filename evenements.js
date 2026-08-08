addd7225efce4f74e6586a1728cfaca342c13767
anjarazulu <anjara26s.zulu@gmail.com>
2026-08-08 22:44:23 +0300

    Update event photos and descriptions in evenements.js

--- evenements.js
+++ evenements.js
@@ -4,7 +4,7 @@ const evenements = [
         titre: "Création du Feo Iray Gospel",
         date: "08 Mai 2024",
         statut: "passe",
-        photo: "images/event1.jpg",
+        photo: "[images/logo.png, images/nicolas.jpg, images/dej3.jpeg]",
         description: "«...mba ho iray ihany izy rehetra», fivoriana voalohany tao amin'ny Amphithéâtre an'ny Lycée Privée Catholique Saint Jean de Matha ny coach Arson Nicolas no nampianatra ireo membres voalohany ny foto-kevitra «Gospel». Tamin'izany ny anaran'ny tarika dia mbola hoe «Origins Choir»."
     },
     {
@@ -12,7 +12,7 @@ const evenements = [
         titre: "Déjeuner concert au St Jean de Matha",
         date: "31 Mai 2024",
         statut: "passe",
-        photo: "images/djn.jpeg",
+        photo: "[images/dej1.jpeg, images/dej3.jpeg, images/dej4.jpeg, images/dej5.jpeg, images/dej6.jpeg]",
         description: "Nandray anjara tamin'ny déjeuner concert izay nataon'ny Lycée St Jean de Matha ny Feo Iray"
     },
     {
@@ -20,7 +20,7 @@ const evenements = [
         titre: "Fampisehoana alina nialohan'ny fetim-pirenena",
         date: "25 juin 2024",
         statut: "passe",
-        photo: "images/event2.jpg",
+        photo: "[images/25a.jpg, images/25c.jpeg, images/25b.jpg, images/25d.jpeg]",
         description: "Nandray anjara tamin'ny fampisehoana nandritra ny varotra teo amin'ny Fafah Botika nandritra ny alina nialohan'ny fetim-pirenena ny tarika."
     },
     {
@@ -28,7 +28,47 @@ const evenements = [
         titre: "Répétitions",
         date: "Grandes vacances 2024",
         statut: "passe",
-        photo: "images/reps.jpeg",
-        description: "Nandray anjara tamin'ny déjeuner concert izay nataon'ny Lycée St Jean de Matha ny Feo Iray"
+        photo: "[images/reps1b.jpg, images/reps1c.jpg, images/reps1d.jpg, images/reps1a.jpg]",
+        description: "Famerenana sy fianarana natao nandritra ny fialan-tsasatra ary mba ho fanomanana ny fotoan-dehibe izay nataon'ny tarika"
+    },
+    {
+        id: 5,
+        titre: "Premier concert",
+        date: "08 Septembre 2024",
+        statut: "passe",
+        photo: "[images/con4.jpeg, images/con3.jpeg, images/con2.jpeg, images/con1.jpeg]",
+        description: "Konseritra lehibe voalohany natokana ho an'ny tarika ka nanasongadinana izay efa vita rehetra nandritra izay 4 volana izay."
+    },
+    {
+        id: 6,
+        titre: "Fandraisana an'i Anjara sy Miaro",
+        date: "01 Décembre 2024",
+        statut: "passe",
+        photo: "[images/am1.jpg, images/am3.jpeg, images/am4.jpeg, images/am5.jpeg, images/am2.jpeg]",
+        description: "Noraisina ho mpandray fanasan'ny Tompo Anjara sy Miaro ka ny Feo Iray dia isan'ireo voaasa, ary koa nanafana ny sehatra"
+    },
+    {
+        id: 7,
+        titre: "Répétitions",
+        date: "Grandes vacances 2025",
+        statut: "passe",
+        photo: "[images/reps2a.jpg]",
+        description: "Famerenana sy fianarana natao nandritra ny fialan-tsasatra mba hamerena izay efa hay ary hanamafisana izany"
+    },
+    {
+        id: 8,
+        titre: "Retrouvailles",
+        date: "06 Avril 2026",
+        statut: "passe",
+        photo: "[images/ret1.jpeg, images/ret2.jpeg, images/ret3.jpeg, images/ret4.jpeg, images/ret5.jpeg, images/ret6.jpeg, images/ret7.jpeg]",
+        description: "Fiaraha-misakafo nataon'ny tarika indray taorian'ny nandehanan'i Anjara sy Nicolas"
+    },
+    {
+        id: 9,
+        titre: "Préparation",
+        date: "Grandes vacances 2026",
+        statut: "passe",
+        photo: "[images/reps3a.jpeg]",
+        description: "Famerenana ary fanomanana ny konseritra izay ho ataon'ny Feo Iray indray amin'ny 06 Septambra 2026"
     },
 ];
