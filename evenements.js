5ca61589461e4ea2ef80227bad9b39ce88d1ee70
anjarazulu <anjara26s.zulu@gmail.com>
2026-08-08 17:25:14 +0300

    Add events data to evenements.js

---
+++ evenements.js
@@ -0,0 +1,18 @@
+const evenements = [
+    {
+        id: 1,
+        titre: "Création du Feo Iray Gospel",
+        date: "08 Mai 2024",
+        statut: "passe",
+        photo: "images/evenement1.jpg",
+        description: "«...mba ho iray ihany izy rehetra», fivoriana voalohany tao amin'ny Amphithéâtre an'ny Lycée Privée Catholique Saint Jean de Matha ny coach Arson Nicolas no nampianatra ireo membres voalohany ny foto-kevitra «Gospel». Tamin'izany ny anaran'ny tarika dia mbola hoe «Origins Choir»."
+    },
+    {
+        id: 2,
+        titre: "Fampisehoana alina nialohan'ny fetim-pirenena",
+        date: "25 juin 2024",
+        statut: "passe",
+        photo: "images/evenement2.jpg",
+        description: "Nandray anjara tamin'ny fampisehoana nandritra ny varotra teo amin'ny Fafah Botika nandritra ny alina nialohan'ny fetim-pirenena ny tarika."
+    },
+];
